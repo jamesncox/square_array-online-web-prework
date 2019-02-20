50f9f4cdaa455af5ed4004e661d7b474ff9ad4bf
James Cox <jamesncox@yahoo.com>
2019-02-20 01:06:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   newArray = []
   array.each do |numbers|
     numbers = numbers**2
-    newArray<<
+    newArray<<numbers
   numbers = [1,2,3]
   array.each do |new_numbers|
     numbers << new_numbers ** 2
